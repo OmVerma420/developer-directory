1ad7b442495ddd5a19a64030d06c205cacc77efa
OmVerma420 <varmaom956@gmail.com>
2025-12-04 02:19:27 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -18,5 +18,4 @@ connectDB();
 // Routes
 app.use("/developers", developerRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
