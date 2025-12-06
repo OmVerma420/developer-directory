c38c71ab3e76df8e5012f74835f962aae97bd661
OmVerma420 <varmaom956@gmail.com>
2025-12-06 20:25:21 +0530

    remove

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,4 @@ app.get("/", (req, res) => {
 // DB
 connectDB();
 
-// ⭐ REQUIRED for Render deployment
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
